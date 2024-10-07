b1483bca6cd7c16b0353be2af4b628d438089f48
Johnleonarduc <johnleonarduc@gmail.com>
2024-10-07 10:56:21 +0100

    bootstrap reat app

---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+const HomePage = () => {
+    return (
+        <h1 className='grid grid-rows-[1fr_2fr]'>
+            This is my home page.
+        </h1>
+    );
+};
+
+export default HomePage;
